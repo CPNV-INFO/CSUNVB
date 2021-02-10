b1f67bc183e7a1655a48d0700e76c8e607a1c0a4
Unknown <Xavier.Carrel@cpnv.ch>
2021-02-10 14:55:21 +0100

    Merge TGR branch

--- public/js/drugs.js
+++ public/js/drugs.js
@@ -15,21 +15,40 @@ function sendData() {
 }
 
 function drugCheck(UID) {
-    let expectedAmount = Number(document.getElementById(UID + "start").value);
-    let endAmount = Number(document.getElementById(UID + "end").value);
+    if(drugsheetmode !== "close") {
+        let expectedAmount = Number(document.getElementById(UID + "start").value);
+        let endAmount = Number(document.getElementById(UID + "end").value);
 
-    //pharmacheck?
-    if(UID.indexOf("pharma") !== -1) {
-        let novaCells = document.querySelectorAll("." + UID + ".nova");
-        //not cells.forEach because then no way to get value out of callback function
-        for(let i = 0; i < novaCells.length; i++) {
-            expectedAmount -= Number(novaCells[i].value);
+        //pharmacheck?
+        if (UID.indexOf("pharma") !== -1) {
+            let novaCells = document.querySelectorAll("." + UID + ".nova");
+            //not cells.forEach because then no way to get value out of callback function
+            for (let i = 0; i < novaCells.length; i++) {
+                expectedAmount -= Number(novaCells[i].value);
+            }
         }
-    }
-    if(endAmount !== expectedAmount) {
-        document.getElementById(UID).style = "background-color: orange;"
-    }
-    else {
-        document.getElementById(UID).removeAttribute("style");
+        if (endAmount !== expectedAmount) {
+            document.getElementById(UID).style = "background-color: orange;"
+        } else {
+            document.getElementById(UID).removeAttribute("style");
+        }
+    }else{
+        let expectedAmount = Number(document.getElementById(UID + "start").textContent);
+        let endAmount = Number(document.getElementById(UID + "end").textContent);
+
+        //pharmacheck?
+        if (UID.indexOf("pharma") !== -1) {
+            let novaCells = document.querySelectorAll("." + UID + ".nova");
+            //not cells.forEach because then no way to get value out of callback function
+            for (let i = 0; i < novaCells.length; i++) {
+                expectedAmount -= Number(novaCells[i].textContent);
+            }
+        }
+
+        if (endAmount !== expectedAmount) {
+            document.getElementById(UID).style = "background-color: orange;"
+        } else {
+            document.getElementById(UID).removeAttribute("style");
+        }
     }
 }
--- view/drugs/show.php
+++ view/drugs/show.php
@@ -7,7 +7,11 @@
 $title = "CSU-NVB - Stupéfiants";
 ob_start();
 ?>
+<script>
+    var drugsheetmode = '<?= $drugsheet["slug"]; ?>';
+</script>
 <script src="js/drugs.js"></script>
+
 <div>
     <h1>Stupéfiants</h1>
     <h2>Semaine <?= $drugsheet["week"] ?> - Base de <?= $site['name'] ?> [<?= $drugsheet['displayname'] ?>]</h2>
@@ -50,55 +54,79 @@ ob_start();
                 <td class="font-weight-bold"><?= $drug["name"] ?></td>
                 <td></td>
                 <?php foreach ($novas as $nova): ?>
-                    <?php $ncheck = getNovaCheckByDateAndDrug($date, $drug['id'], $nova['id'], $drugsheet['id']); // not great practice, but it spares repeated queries on the db ?>
+                    <?php $ncheck = getNovaCheckByDateAndDrug($date, $drug['id'], $nova['id'], $drugsheet['id']); // not great practice, but it spares repeated queries on the db
+                    if($ncheck == false) $ncheck = array("start" => 0,"end"=>0);
+                    ?>
                     <?php $UID = 'n' . $nova["number"] . 'd' . $drug["id"] . 'D' . $date ?>
+                    <?php if($drugsheet['slug'] != "close"): // We check if the drugsheet is closed or not to change input to simple div ?>
                     <td id="<?= $UID ?>">
                         <input  type="number" min="0" class="text-center"
                                 value="<?= (is_numeric($ncheck["start"]) ? $ncheck["start"] : '0') ?>"
                                 onchange="cellUpdate('<?= $UID ?>', 'start');"
                                 id="<?= $UID ?>start"
-                                <?= ($drugsheet['slug'] == "close") ? "readonly" : '' ?>
                         >
                         <input  type="number" min="0" class="text-center"
                                 value="<?= (is_numeric($ncheck["end"]) ? $ncheck["end"] : '0') ?>"
                                 onchange="cellUpdate('<?= $UID ?>', 'end');"
                                 id="<?= $UID ?>end"
-                                <?= ($drugsheet['slug'] == "close") ? "readonly" : '' ?>
                     >
                     </td>
+                    <?php else: ?>
+                    <td id="<?= $UID ?>" class="text-center">
+                        <div id="<?= $UID ?>start" class="w-25 d-inline"><?= (is_numeric($ncheck["start"]) == true ? $ncheck["start"] : '0') ?></div> / <div id="<?= $UID ?>end" class="w-25 d-inline"><?= (is_numeric($ncheck["end"]) == true ? $ncheck["end"] : '0') ?></div>
+                    </td>
+                    <?php endif; ?>
                 <?php array_push($UIDs, $UID); ?>
                 <?php endforeach; ?>
                 <td></td>
             </tr>
             <?php foreach ($batchesByDrugId[$drug["id"]] as $batch): ?>
                 <?php $UID = "pharma_" . 'b' . $batch['id'] . 'D' . $date ?>
-                <?php $pcheck = getPharmaCheckByDateAndBatch($date, $batch['id'], $drugsheet['id']); ?>
+                <?php $pcheck = getPharmaCheckByDateAndBatch($date, $batch['id'], $drugsheet['id']);
+                if($pcheck == false) $pcheck = array("start" => 0,"end"=>0);
+                ?>
                 <tr>
                     <td class="text-right"><?= $batch['number'] ?></td>
                     <td class="text-center">
+                        <?php if($drugsheet['slug'] != "close"): // We check if the drugsheet is closed or not to change input to simple div?>
                         <input  type="number" min="0" class="text-center"
                                 value="<?= (is_numeric($pcheck['start']) ? $pcheck['start'] : '0') ?>"
                                 onchange="cellUpdate('<?= $UID ?>', 'start');"
                                 id="<?= $UID ?>start"
-                                <?= ($drugsheet['slug'] == "close") ? "readonly" : '' ?>
+
                         >
                     </td>
+                    <?php else: ?>
+                        <div class="text-center" id="<?= $UID ?>start"><?= (is_numeric($pcheck['start']) ? $pcheck['start'] : '0') ?></div>
+
+                    <?php endif; ?>
                     <?php foreach ($novas as $nova): ?>
                         <td class="text-center">
-                            <input type="number" min="0" class="<?= $UID ?> nova text-center"
-                                    value="<?= (getRestockByDateAndDrug($date, $batch['id'], $nova['id']) + 0) //+0 auto converts to a number, even if null ?>"
-                                    onchange="cellUpdate('<?= $UID ?>')"
-                                    <?= ($drugsheet['slug'] == "close") ? "readonly" : '' ?>
-                            >
+                            <?php if ($drugsheet['slug'] != "close"): ?>
+                                <input type="number" min="0" class="<?= $UID ?> nova text-center"
+                                        value="<?= (getRestockByDateAndDrug($date, $batch['id'], $nova['id']) + 0) //+0 auto converts to a number, even if null ?>"
+                                        onchange="cellUpdate('<?= $UID ?>')"
+
+                                >
+                            <?php else: ?>
+                                <div class="<?= $UID ?> nova text-center"><?= (getRestockByDateAndDrug($date, $batch['id'], $nova['id']) ? getRestockByDateAndDrug($date, $batch['id'], $nova['id']) : '0') /*(getRestockByDateAndDrug($date, $batch['id'], $nova['id']) + 0) */ //+0 auto converts to a number, even if null ?></div>
+                            <?php endif; ?>
                         </td>
                     <?php endforeach; ?>
                     <td id="<?= $UID ?>" class="text-center">
-                        <input  type="number" min="0" class="text-center"
-                                value="<?= is_numeric($pcheck['end']) ? $pcheck['end'] : '0'?>" class="text-center"
-                                onchange="cellUpdate('<?= $UID ?>', 'end');"
-                                id="<?= $UID ?>end"
-                                <?= ($drugsheet['slug'] == "close") ? "readonly" : '' ?>
-                        >
+                        <?php if ($drugsheet['slug'] != "close"): ?>
+
+                            <input type="number" min="0" class="text-center"
+                                   value="<?= is_numeric($pcheck['end']) ? $pcheck['end'] : '0' ?>" class="text-center"
+                                   onchange="cellUpdate('<?= $UID ?>', 'end');"
+                                   id="<?= $UID ?>end"
+
+                            >
+                        <?php else: ?>
+
+                            <div class="text-center" id="<?= $UID ?>end"><?= is_numeric($pcheck['end']) ? $pcheck['end'] : '0' ?></div>
+
+                        <?php endif; ?>
                     </td>
                 </tr>
             <?php array_push($UIDs, $UID); ?>
